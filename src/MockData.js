8a1c33f84935cc8e4a953224898fb7b24a84cbe6
olimjonov97 <akon.xan@mail.ru>
2022-02-17 21:41:50 +0500

    modified data

--- src/MockData.js
+++ src/MockData.js
@@ -43,7 +43,7 @@ export const MockData =[
         },
         
     ]
-export const navbar = [
+export const r.navbar = [
   { id: 1, title: 'Home' },
   { id: 2, title: 'About' },
   { id: 3, title: 'Contacts' },
