618dbc559886ec12a4bd5dd2c3022f6def2021ea
olimjonov97 <akon.xan@mail.ru>
2022-03-11 23:35:46 +0500

    log

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -1,10 +1,11 @@
 import React, { useState,createContext } from 'react'
 import { Flowers} from'../Mockdata/MockData'
-export const FlowersContext = createContext()
+export const FlowersContext5 = createContext()
 
 const Flower = (props) => {
     const children = props.children
-    const [data, setData] = useState(Flowers);
+  const [data, setData] = useState(Flowers);
+  console.log(data)
     return (
       <FlowersContext.Provider value={[data, setData]}>
         {children}
